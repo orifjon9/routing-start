850bc01e245a81e792eb33257d6dd379762081f1
orifjon9 <orifjon9@gmail.com>
2017-07-26 20:36:01 -0500

    server & users component changes

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit {
+export class ServerComponent implements OnInit, OnDestroy {
   server: {id: number, name: string, status: string};
+  paramSubscription: Subscription;
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+            private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id: number = +this.route.snapshot.params['id'];
+    this.loadServer(id);
+
+    this.paramSubscription = this.route.params.subscribe((params:Params) =>{
+      this.loadServer(+params['id']);
+    });
+
   }
 
+  ngOnDestroy() {
+    this.paramSubscription.unsubscribe();
+  }
+
+
+  private loadServer(id: number){
+    this.server = this.serversService.getServer(id);
+  }
 }
